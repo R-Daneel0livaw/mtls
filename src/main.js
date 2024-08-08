8ce41b835684c96f835b3fed5d46e2efc49dd915
erich <eric.hine2214@gmail.com>
2024-08-08 15:08:54 -0400

    Clean up main and client

--- src/main.js
+++ src/main.js
@@ -1,25 +1,6 @@
 import { makeRequest } from './client.js';
 import { startServer } from './server.js';
 
-// export function performRequest(serverConfig, requestConfig) {
-//   return new Promise((resolve, reject) => {
-//     const server = startServer(serverConfig);
-
-//     setTimeout(() => {
-//       server.close((err) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           console.log('Server has been closed.');
-//           resolve();
-//         }
-//       });
-//     }, 2000); 
-
-//     makeRequest(requestConfig);
-//   });
-// }
-
 export function performRequest(serverConfig, requestConfig) {
   return new Promise((resolve, reject) => {
     const server = startServer(serverConfig);
