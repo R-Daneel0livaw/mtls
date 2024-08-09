3f003e7cdccaa318c8003d547e2de660cc775a81
erich <eric.hine2214@gmail.com>
2024-08-09 14:27:00 -0400

    Add second test which includes intermediate

--- test/mTLS.test.js
+++ test/mTLS.test.js
@@ -1,28 +1,55 @@
 import { expect } from 'chai';
 import { performRequest } from '../src/main.js';
 
-describe('performRequest Function Test', function () {
+describe('mTLS Tests', function () {
   this.timeout(5000);
 
-  const serverConfig = {
-    port: 8443,
-    serverCert: 'server.crt',
-    serverKey: 'server.key',
-    caCerts: ['ca.pem'],
-    scenario: 1
-  }
 
-  const requestConfig = {
-    host: 'localhost',
-    port: 8443,
-    path: '/',
-    clientCert: 'client.crt',
-    clientKey: 'client.key',
-    caCerts: ['ca.pem'],
-    scenario: 1
-  }
+  it('should receive the expected mTLS message and status for client and server cert with common root', function (done) {
+    const serverConfig = {
+      port: 8443,
+      serverCert: 'server.crt',
+      serverKey: 'server.key',
+      caCerts: ['ca.pem'],
+      scenario: 1
+    }
 
-  it('should receive the expected mTLS message and status for client and server cert with common root', function(done) {
+    const requestConfig = {
+      host: 'localhost',
+      port: 8443,
+      path: '/',
+      clientCert: 'client.crt',
+      clientKey: 'client.key',
+      caCerts: ['ca.pem'],
+      scenario: 1
+    }
+
+    performRequest(serverConfig, requestConfig).then((response) => {
+      expect(response.statusCode).to.equal(200);
+      expect(response.message).to.equal('Hello, mutual TLS client!');
+      done();
+    }).catch(done);
+  });
+
+  it('should receive the expected mTLS message and status for client and server cert, intermediate and root', function (done) {
+    const serverConfig = {
+      port: 8443,
+      serverCert: 'server-chain.crt',
+      serverKey: 'server.key',
+      caCerts: ['rootCA.pem'],
+      scenario: 2
+    }
+
+    const requestConfig = {
+      host: 'localhost',
+      port: 8443,
+      path: '/',
+      clientCert: 'client-chain.crt',
+      clientKey: 'client.key',
+      caCerts: ['rootCA.pem'],
+      scenario: 2
+    }
+
     performRequest(serverConfig, requestConfig).then((response) => {
       expect(response.statusCode).to.equal(200);
       expect(response.message).to.equal('Hello, mutual TLS client!');
